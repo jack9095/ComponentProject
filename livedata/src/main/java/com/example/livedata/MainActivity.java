d1879e43ca7b2c2003483a877d42ce2bf5aa5d62
wangfei <gaodun123com>
2018-08-31 14:23:40 +0800

    Initial commit

---
+++ livedata/src/main/java/com/example/livedata/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.livedata;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * https://blog.csdn.net/guiying712/article/details/55213884
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
